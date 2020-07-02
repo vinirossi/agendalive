cecda03927c1f64fb29d81d8a8721d9eb2dc6f9a
Vinicius Rossi <vini.rossisilva@gmail.com>
2020-07-02 12:18:58 -0300

    Adicionado o Component Card do Angular Material e Instalado o Flex Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
 
 // Components do Angular Material
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -28,6 +29,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule
